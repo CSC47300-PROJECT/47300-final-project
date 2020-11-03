9817bfe125c8155d1f484c53e169d36b63c2e73c
yifengjin89 <55426354+yifengjin89@users.noreply.github.com>
2020-11-02 19:21:44 -0500

    sonar test

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const multer = require('multer')
-// const GridFsStorage = require("multer-gridfs-storage")
-// const Grid = require('gridfs-stream')
-// const path = require('path')
 const methodOverride = require('method-override')
 const cors = require('cors')
-// const crypto = require("crypto")
 const favicon = require('serve-favicon')
 
 // create server
